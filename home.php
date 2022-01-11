ff7aed29e73299d0d300b387c77e579caeb3a2a2
unknown <shahzeb.naqvi12@gmail.com>
2022-01-12 00:31:26 +0500

    homepage

--- home.php
+++ home.php
@@ -20,116 +20,83 @@ include_once("includes/function.php");
 	<![endif]-->
 		<link rel="stylesheet" href="jquery-ui.css">
         <link rel="stylesheet" href="bootstrap.min.css" />
-		<script src="jquery.min.js"></script>  
-		<script src="jquery-ui.js"></script>
-</head>
+        <script src="jquery.min.js"></script>  
+        <script src="jquery-ui.js"></script>
+    </head>
 
-<body>
-    <div class="main-wrapper">
-      <?php include_once("includes/header.php");?>
+    <body>
+        <div class="main-wrapper">
+          <?php include_once("includes/header.php");?>
 
-              <?php include_once("includes/sidebar.php");?>
+          <?php include_once("includes/sidebar.php");?>
 
-        <div class="page-wrapper">
+          <div class="page-wrapper">
             <div class="content container-fluid">
-             
+
+
                 
+
+
+
+
+
+
+
+
+
+
+
+
+
                 <div class="row">
-                    
-                    <div class="col-lg-12">
+
+                    <div class="col-lg-4">
                         <div class="card-box">
                             <div class="card-block">
                                 <h4 class="card-title">Responsive Tables</h4>
                                 <div class="table-responsive">
-                                    <table class="table">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Firstname</th>
-                                                <th>Lastname</th>
-                                                <th>Age</th>
-                                                <th>City</th>
-                                                <th>Country</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>Anna</td>
-                                                <td>Pitt</td>
-                                                <td>35</td>
-                                                <td>New York</td>
-                                                <td>USA</td>
-                                            </tr>
-                                          
-                                        </tbody>
-                                    </table>
-                                </div>
+                                    <form action="" id="myform">
+                                      <div class="form-group">
+                                        <input type="hidden" name="" id="stuid">
+                                        <label for="nameid">Name</label>
+                                        <input type="text" class="form-control" id="nameid">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="emailid" class="form-label">Email</label>
+                                        <input type="email" class="form-control" id="emailid">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="passwordid" class="form-label">Password</label>
+                                        <input type="password" class="form-control" id="passwordid">
+                                    </div>
+                                    <div class="text-right">
+                                        <button type="submit" class="btn btn-primary" id="btnsave">Save</button>
+                                    </div>
+                                    <div id="msg"></div>
+                                </form> </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-<div class="row">
-                    
-                    <div class="col-lg-12">
+                    <div class="col-lg-8">
                         <div class="card-box">
                             <div class="card-block">
                                 <h4 class="card-title">Responsive Tables</h4>
-                                <div class="table-responsive">
-                                    <form action="" class="col-sm-5" id="myform">
-          <h3 class="alert-warning text-center p-2">Registration</h3>
-          <div class="form-group">
-            <input type="hidden" name="" id="stuid">
-            <label for="nameid">Name</label>
-            <input type="text" class="form-control" id="nameid">
+                                <table class="table">
+                                  <thead>
+                                    <tr>
+                                      <th>ID</th>
+                                      <th>Name</th>
+                                      <th>Email</th>
+                                      <th>Password</th>
+                                      <th>Action</th>
+                                  </tr>
+                              </thead>
+                              <tbody id="tbody"></tbody>
+                          </table>
+                      </div>
+                  </div>
+              </div>
           </div>
-          <div class="form-group">
-            <label for="emailid" class="form-label">Email</label>
-            <input type="email" class="form-control" id="emailid">
-          </div>
-          <div class="form-group">
-            <label for="passwordid" class="form-label">Password</label>
-            <input type="password" class="form-control" id="passwordid">
-          </div>
-          <div class="text-right">
-            <button type="submit" class="btn btn-primary" id="btnsave">Save</button>
-          </div>
-          <div id="msg"></div>
-        </form>
-        <div class="col-sm-7 text-center">
-        <h3 class="alert-warning text-center p-2">Show Data</h3>
-        <table class="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Password</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody id="tbody"></tbody>
-        </table>
-        </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
 
 
@@ -149,22 +116,20 @@ include_once("includes/function.php");
 
 
 
-
-
-            </div>
+      </div>
 
 
       <?php include_once("includes/notification.php");?>
-        </div>
-    </div>
-    <div class="sidebar-overlay" data-reff=""></div>
-    <script type="text/javascript" src="assets/js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.slimscroll.js"></script>
-    <script type="text/javascript" src="assets/js/app.js"></script>
+  </div>
+</div>
+<div class="sidebar-overlay" data-reff=""></div>
+<script type="text/javascript" src="assets/js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.slimscroll.js"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="js/jqajax.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="js/jqajax.js"></script>
 
 </body>
 
